eceeaa0b892db55a3130ba05691a84438efe0894
Deniz <s0571123@htw-berlin.de>
2025-04-21 16:49:28 +0200

    init

---
+++ src/main/java/de/businesscards/VisitenkartenWebtechApplication.java
@@ -0,0 +1,13 @@
+package de.businesscards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitenkartenWebtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitenkartenWebtechApplication.class, args);
+	}
+
+}
